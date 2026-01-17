dd0660f5ab3c50a270154a496c133d48d7a3bb1d
yemin917 <kye030307@gmail.com>
2026-01-17 22:00:05 +0900

    Update uart_ctrl.v

--- src/integrating_tx_rx/uart_ctrl.v
+++ src/integrating_tx_rx/uart_ctrl.v
@@ -1,101 +1,39 @@
-module uart_tx #(
-    parameter CLK_FREQ = 50_000_000,
-    parameter BAUD_RATE = 9600,
-    parameter IDLE = 3'b000,
-    parameter START = 3'b001,
-    parameter DATA = 3'b010,
-    parameter PARITY = 3'b011,
-    parameter STOP = 3'b100
-)(
+module uart_ctrl (
     input clk, rstn,
-    input tx_start,
-    input parity_mode,
-    input [7:0] tx_data,
-    output reg parity_bit,
-    output reg tx_line,
-    output reg tx_busy
-);
-
-    reg [2:0] state;
-    reg [12:0]baud_cnt;
-    reg [3:0] bit_idx;
-    reg [7:0] shift_reg;
     
-    localparam BAUD_CNT_MAX = CLK_FREQ / BAUD_RATE;
-
-    //FSM
+    input [7:0] rx_data,
+    input rx_done, rx_error,
+    
+    output reg [7:0] tx_data,
+    output reg tx_start,
+    
+    input tx_busy
+);
+    
+    reg tx_start_flag;
+    
     always @(posedge clk or negedge rstn) begin
-        if(!rstn) begin
-            state <= IDLE;
-            tx_line <= 1;
-            tx_busy <= 0;
-            baud_cnt <= 0;
-            bit_idx <= 0;
-            parity_bit <= 'h0;
-        end
-        else begin
-            case (state)
-                IDLE : begin
-                    if(tx_start) begin
-                        shift_reg <= tx_data;
-                        parity_bit <= (parity_mode)? ~^tx_data : ^tx_data;
-                        tx_line <= 0;
-                        tx_busy <= 1;
-                        baud_cnt <= 0;
-                        bit_idx <= 0;
-                        state <= START;
-                    end
-                end
-                START : begin
-                    if(baud_cnt == BAUD_CNT_MAX-1) begin
-                        baud_cnt <= 0;
-                        tx_line <= shift_reg[0];
-                        shift_reg <= shift_reg >> 1;
-                        bit_idx <= 1;
-                        state <= DATA;
-                    end
-                    else begin
-                        baud_cnt <= baud_cnt + 1;
-                    end
-                end
-                DATA : begin
-                    if(baud_cnt == BAUD_CNT_MAX-1) begin
-                        baud_cnt <= 0;
-                        if(bit_idx < 8) begin
-                            tx_line <= shift_reg[0];
-                            shift_reg <= shift_reg >> 1;
-                            bit_idx <= bit_idx + 1;
-                        end
-                        else begin
-                            tx_line <= parity_bit;
-                            state <= PARITY;
-                        end
-                    end
-                    else begin
-                        baud_cnt <= baud_cnt + 1;
-                    end
-                end
-                PARITY : begin
-                    if(baud_cnt == BAUD_CNT_MAX-1) begin
-                        baud_cnt <= 0;
-                        tx_line <= 1;
-                        state <= STOP;
-                    end
-                    else begin
-                        baud_cnt <= baud_cnt + 1;
-                    end
-                end
-                STOP:  begin
-                    if(baud_cnt == BAUD_CNT_MAX-1) begin
-                        baud_cnt <= 0;
-                        tx_busy <= 0;
-                        state <= IDLE;
-                    end
-                    else begin
-                        baud_cnt <= baud_cnt + 1;
-                    end
-                end
-            endcase 
-        end
-    end                                             
+        if (~rstn)
+            tx_data <= 'h0;
+        else if (rx_done & ~rx_error)
+            tx_data <= rx_data;
+     end
+     
+     always @(posedge clk or negedge rstn) begin
+        if (~rstn)
+            tx_start_flag <= 'h0;
+        else if (tx_start_flag & tx_start)
+            tx_start_flag <= 'h0;
+        else if (rx_done & ~rx_error)
+            tx_start_flag <= 'h1;
+     end
+     
+     always @(posedge clk or negedge rstn) begin
+        if (~rstn)
+            tx_start <= 'h0;
+        else if (tx_start)
+            tx_start <= 'h0;
+        else if (tx_start_flag & ~tx_busy)
+            tx_start <= 1'b1;
+     end
 endmodule
